b4d350b956417fc9e901a8c3478e76eaf30c4ff9
Rohit Tiwari <166986989+Dedrknex8@users.noreply.github.com>
2025-01-15 12:17:41 +0530

    make some changes

--- database/db.js
+++ database/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectionToDB = async() =>{
     try{
-        await mongoose.connect("mongodb+srv://Dedrknex:Dedrknex001@cluster0.zqctj.mongodb.net/");
+        await mongoose.connect("mongodb+srv://Dedrknex:<yourpaswd>@cluster0.zqctj.mongodb.net/");
         console.log('mongo connected sucessfully');   
     }catch(error){
         console.log("MongoDb connection failed",error);
